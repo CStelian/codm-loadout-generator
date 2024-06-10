d10f01ccd0fa93f68a0d563cfb0509ba11a570cc
Stelian Cioarec <cioarecstelian@gmai.com>
2024-06-10 21:27:47 +0300

    first commit

---
+++ src/data/loadouts/loadouts.ts
@@ -0,0 +1,193 @@
+const codmWeaponTypes = [
+    'assault',
+    'sniper',
+    'lmg',
+    'smg',
+    'shotgun',
+    'marksman',
+    'pistol',
+    'launcher',
+    'melee',
+]
+
+const assault = [
+    'M4',
+    'Type 25',
+    'M16',
+    'AK117',
+    'AK-47',
+    'ASM10',
+    'BK57',
+    'LK24',
+    'ICR-1',
+    'Man-O-War',
+    'KN-44',
+    'HBRa3',
+    'HVK-30',
+    'DR-H',
+    'Peacekeeper MK2',
+    'FR .556',
+    'AS VAL',
+    'CR-56 AMAX',
+    'M13',
+    'Swordfish',
+    'Kilo 141',
+    'Oden',
+    'Krig 6',
+    'EM2',
+    'Maddox',
+    'FFAR 1',
+    'Grau 5.56',
+    'Groza',
+    'Type 19'
+]
+
+const sniper = [
+    'XPR-50',
+    'Arctic .50',
+    'M21 EBR',
+    'DL Q33',
+    'Locus',
+    'NA-45',
+    'Outlaw',
+    'Rytec AMR',
+    'SVD',
+    'Koshka',
+    'ZRG 20mm',
+    'HDR',
+    'LW3-Tundra'
+]
+
+const lmg = [
+    'S36',
+    'UL736',
+    'RPD',
+    'M4LMG',
+    'Chopper',
+    'Holger 26',
+    'Hades',
+    'PKM',
+    'Dingo',
+    'Bruen MK9',
+    'MG42'
+]
+
+const smg = [
+    'RUS-79U',
+    'Chicom',
+    'PDW-57',
+    'Razorback',
+    'MSMC',
+    'HG 40',
+    'Pharo',
+    'GKS',
+    'Cordite',
+    'QQ9',
+    'Fennec',
+    'AGR 556',
+    'QXR',
+    'PP19 Bizon',
+    'MX9',
+    'CBR4',
+    'PPSh-41',
+    'MAC-10',
+    'KSP 45',
+    'Switchblade X9',
+    'LAPA',
+    'OTs 9',
+    'Striker 45',
+    'CX-9',
+    'TEC-9'
+]
+
+const shotgun = [
+    'HS2126',
+    'BY15',
+    'HS0405',
+    'Striker',
+    'KRM-262',
+    'Echo',
+    'R9-0',
+    'JAK-12',
+    'Argus'
+]
+
+const marksman = [
+    'Kilo Bolt-Action',
+    'SKS',
+    'SP-R 208',
+    'MK2'
+]
+
+const pistol = [
+    'J358',
+    'MW11',
+    '.50 GS',
+    'Renetti',
+    'Shorty',
+    'Crossbow',
+    'L-CAR 9',
+    'Dobvra',
+    'Nail Gun',
+    'Machine Pistol'
+]
+
+const launcher = [
+    'FHJ-18',
+    'SMRS',
+    'Thumper',
+    'D13 Sector'
+]
+
+const melee = [
+    'Base Melee',
+    'Knife',
+    'Axe',
+    'Shovel',
+    'Baseball Bat',
+    'Nunchucks',
+    'Katana',
+    'Prizefighters',
+    'Wrench',
+    'Sickle',
+    'Machete',
+    'Kali Sticks',
+    'Butterfly Knife',
+    'Sai',
+    'Ballistic Knife',
+    'Spear'
+]
+
+const attachmentTypes = [
+    'muzzle',
+    'barrel',
+    'optic',
+    'stock',
+    'perk',
+    'laser',
+    'underbarrel',
+    'ammunition',
+    'rearGrip'
+]
+
+const primaryWeapons = [
+    assault,
+    sniper,
+    lmg,
+    smg,
+    shotgun,
+    marksman
+]
+
+const secondaryWeapons = [
+    pistol,
+    launcher,
+    melee
+]
+
+export {
+    codmWeaponTypes,
+    primaryWeapons,
+    secondaryWeapons,
+    attachmentTypes
+}
---
+++ src/data/seo/siteSeo.ts
@@ -0,0 +1,8 @@
+export const siteSEO = {
+    title: 'CoD: Mobile Random Loadout Generator',
+    description: 'Generate random loadouts for Call of Duty: Mobile',
+    url: 'https://generator.codmdb.com',
+    image: 'https://generator.codmdb.com/images/og-image.jpg',
+    twitter: '@codmdb',
+    keywords: 'codm, cod mobile, call of duty, random loadout, generator'
+}
